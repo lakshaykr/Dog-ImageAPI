ccbcd9f6fc140c9bf4129d58d75fbde833f30574
lakshayk <76809170+lakshaykr@users.noreply.github.com>
2025-07-06 01:44:41 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,32 +1,37 @@
 from flask import Flask, Response
 import requests
 import random
+import time
 
 app = Flask(__name__)
 
 def get_dog_image():
-    res = requests.get("https://dog.ceo/api/breeds/image/random")
-    return res.json()["message"]
+    time.sleep(0.2)
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
+    return response.json()["message"]
 
 def get_cat_image():
-    res = requests.get("https://api.thecatapi.com/v1/images/search")
-    return res.json()[0]["url"]
+    time.sleep(0.2)
+    response = requests.get("https://api.thecatapi.com/v1/images/search")
+    return response.json()[0]["url"]
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def random_image(path):
-    path = path.lower()
+def serve_image(path):
+    path = path.strip().lower()
 
     if path == "dog":
         image_url = get_dog_image()
     elif path == "cat":
         image_url = get_cat_image()
     else:
-        image_url = random.choice([
-            get_dog_image(),
-            get_cat_image()
-        ])
+        image_url = random.choice([get_dog_image(), get_cat_image()])
 
+
+    return Response(image_url, mimetype="text/plain")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
     return Response(image_url, mimetype="text/plain")
 
 if __name__ == '__main__':
