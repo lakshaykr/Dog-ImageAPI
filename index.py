bf1f3c364ab32be60bb421e0bec42d459ac5b362
lakshayk <76809170+lakshaykr@users.noreply.github.com>
2025-07-05 21:23:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,32 @@
 from flask import Flask, Response
 import requests
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+def get_dog_image():
     res = requests.get("https://dog.ceo/api/breeds/image/random")
-    image_url = res.json()["message"]
+    return res.json()["message"]
+
+def get_cat_image():
+    res = requests.get("https://api.thecatapi.com/v1/images/search")
+    return res.json()[0]["url"]
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def random_image(path):
+    path = path.lower()
+
+    if path == "dog":
+        image_url = get_dog_image()
+    elif path == "cat":
+        image_url = get_cat_image()
+    else:
+        image_url = random.choice([
+            get_dog_image(),
+            get_cat_image()
+        ])
+
     return Response(image_url, mimetype="text/plain")
 
 if __name__ == '__main__':
